8300d5a577f3f6ef60520b6ca3dd391e73c4f4b7
nickdferrara <nick.d.ferrara@gmail.com>
2023-03-23 22:39:06 -0400

    Initial check in

---
+++ src/main/kotlin/com/nickdferrara/Application.kt
@@ -0,0 +1,15 @@
+package com.nickdferrara
+
+import io.ktor.server.application.*
+import com.nickdferrara.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
