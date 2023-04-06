fd078ae6e91c3f157d26a7aebfd93e2ebaee9799
nickdferrara <nick.d.ferrara@gmail.com>
2023-04-05 21:22:06 -0400

    Make user basic crud routes

--- src/main/kotlin/com/nickdferrara/routes/UserRoutes.kt
+++ src/main/kotlin/com/nickdferrara/routes/UserRoutes.kt
@@ -1,8 +1,13 @@
 package com.nickdferrara.routes
 
 import com.nickdferrara.dto.UserDto
+import com.nickdferrara.dto.WorkoutDto
+import com.nickdferrara.extension.toDto
 import com.nickdferrara.extension.toUser
+import com.nickdferrara.extension.toWorkout
 import com.nickdferrara.models.ErrorResponse
+import com.nickdferrara.models.User
+import com.nickdferrara.models.Workout
 import com.nickdferrara.service.UserService
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -25,6 +30,48 @@ fun Route.userRoutes(
                     call.response.headers.append("My-User-Id-Header", userId.toString())
                     call.respond(HttpStatusCode.Created)
                 } ?: call.respond(HttpStatusCode.BadRequest, ErrorResponse.BAD_REQUEST_RESPONSE)
+        }
+
+        get {
+            val userList =
+                service.findAll()
+                    .map(User::toDto)
+            call.respond(userList)
+        }
+
+        get("/{id}") {
+            val id = call.parameters["id"].toString()
+            service.findById(id)
+                ?.let { foundUser -> call.respond(foundUser.toDto()) }
+                ?: call.respond(HttpStatusCode.NotFound,
+                    ErrorResponse.NOT_FOUND_RESPONSE)
+        }
+
+        put("/{id}") {
+            val id = call.parameters["id"].toString()
+            val userRequest = call.receive<UserDto>()
+            val user = userRequest.toUser()
+
+            val updatedSuccessfully = service.updateById(id, user)
+
+            if (updatedSuccessfully) {
+                call.respond(HttpStatusCode.NoContent)
+            } else {
+                call.respond(HttpStatusCode.BadRequest,
+                    ErrorResponse.BAD_REQUEST_RESPONSE)
+            }
+        }
+
+        delete("/{id}") {
+            val id = call.parameters["id"].toString()
+
+            val deletedSuccessfully = service.deleteById(id)
+
+            if (deletedSuccessfully) {
+                call.respond(HttpStatusCode.NoContent)
+            } else {
+                call.respond(HttpStatusCode.NotFound, ErrorResponse.NOT_FOUND_RESPONSE)
+            }
         }
     }
 }
--- src/main/kotlin/com/nickdferrara/service/UserService.kt
+++ src/main/kotlin/com/nickdferrara/service/UserService.kt
@@ -1,8 +1,13 @@
 package com.nickdferrara.service
 
 import com.nickdferrara.models.User
+import com.nickdferrara.models.Workout
+import org.bson.types.ObjectId
 import org.litote.kmongo.Id
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.coroutine.replaceOne
+import org.litote.kmongo.eq
+import org.litote.kmongo.id.toId
 
 class UserService(
     database: CoroutineDatabase
@@ -12,5 +17,35 @@ class UserService(
     suspend fun create(user: User): Id<User>?  {
         userCollection.insertOne(user)
         return user.id
+    }
+
+    suspend fun findAll(): List<User> =
+        userCollection.find()
+            .toList()
+
+    suspend fun findById(id: String): User? {
+        val bsonId: Id<User> = ObjectId(id).toId()
+        return userCollection
+            .findOne(User::id eq bsonId)
+    }
+
+    suspend fun updateById(id: String, request: User): Boolean =
+        findById(id)
+            ?.let { user ->
+                val updateResult = userCollection.replaceOne(
+                    user.copy(
+                        person = request.person,
+                        credential = request.credential,
+                        address = request.address,
+                        membership = request.membership,
+                        profileImage = request.profileImage
+                    )
+                )
+                updateResult.modifiedCount == 1L
+            } ?: false
+
+    suspend fun deleteById(id: String): Boolean {
+        val deleteResult = userCollection.deleteOneById(ObjectId(id))
+        return deleteResult.deletedCount == 1L
     }
 }
