24fe9facea0a28004161a98e593401f0c90d0381
nickdferrara <nick.d.ferrara@gmail.com>
2023-04-05 21:07:51 -0400

    Make workout basic crud routes

--- src/main/kotlin/com/nickdferrara/routes/WorkoutRoutes.kt
+++ src/main/kotlin/com/nickdferrara/routes/WorkoutRoutes.kt
@@ -1,8 +1,13 @@
 package com.nickdferrara.routes
 
+import com.nickdferrara.dto.CredentialDto
 import com.nickdferrara.dto.WorkoutDto
+import com.nickdferrara.extension.toCredential
+import com.nickdferrara.extension.toDto
 import com.nickdferrara.extension.toWorkout
+import com.nickdferrara.models.Credential
 import com.nickdferrara.models.ErrorResponse
+import com.nickdferrara.models.Workout
 import com.nickdferrara.service.WorkoutService
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -26,6 +31,48 @@ fun Route.workoutRoutes(
                     call.response.headers.append("My-User-Id-Header", workoutId.toString())
                     call.respond(HttpStatusCode.Created)
                 } ?: call.respond(HttpStatusCode.BadRequest, ErrorResponse.BAD_REQUEST_RESPONSE)
+        }
+
+        get {
+            val workoutList =
+                service.findAll()
+                    .map(Workout::toDto)
+            call.respond(workoutList)
+        }
+
+        get("/{id}") {
+            val id = call.parameters["id"].toString()
+            service.findById(id)
+                ?.let { foundWorkout -> call.respond(foundWorkout.toDto()) }
+                ?: call.respond(HttpStatusCode.NotFound,
+                    ErrorResponse.NOT_FOUND_RESPONSE)
+        }
+
+        put("/{id}") {
+            val id = call.parameters["id"].toString()
+            val workoutRequest = call.receive<WorkoutDto>()
+            val workout = workoutRequest.toWorkout()
+
+            val updatedSuccessfully = service.updateById(id, workout)
+
+            if (updatedSuccessfully) {
+                call.respond(HttpStatusCode.NoContent)
+            } else {
+                call.respond(HttpStatusCode.BadRequest,
+                    ErrorResponse.BAD_REQUEST_RESPONSE)
+            }
+        }
+
+        delete("/{id}") {
+            val id = call.parameters["id"].toString()
+
+            val deletedSuccessfully = service.deleteById(id)
+
+            if (deletedSuccessfully) {
+                call.respond(HttpStatusCode.NoContent)
+            } else {
+                call.respond(HttpStatusCode.NotFound, ErrorResponse.NOT_FOUND_RESPONSE)
+            }
         }
     }
 
