8036df07a519284431fd3431caac9d7e579a40c1
nickdferrara <nick.d.ferrara@gmail.com>
2023-04-07 22:54:09 -0400

    Moved database name to enviornment variable

--- src/main/kotlin/com/nickdferrara/plugins/Database.kt
+++ src/main/kotlin/com/nickdferrara/plugins/Database.kt
@@ -8,9 +8,10 @@ import org.litote.kmongo.reactivestreams.KMongo
 fun Application.configureDatabase(): CoroutineDatabase {
     val mongoUsername = System.getenv("MONGO_USERNAME")
     val mongoPassword = System.getenv("MONGO_PASSWORD")
+    val mongoDbName = System.getenv("MONGO_DBNAME")
     val client = KMongo.createClient(
         connectionString = "mongodb+srv://$mongoUsername:$mongoPassword@cluster0.kg571te.mongodb.net/?retryWrites=true&w=majority"
     ).coroutine
 
-    return client.getDatabase("fitnessapp")
+    return client.getDatabase("$mongoDbName")
 }
