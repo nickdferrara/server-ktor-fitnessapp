f8b9ddffbe2b4ba5ad23dae583aa831ea9eb8649
nickdferrara <nick.d.ferrara@gmail.com>
2023-04-10 21:41:41 -0400

    Added dependencies, removed template code.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 val kmongo_version: String by project
 val java_version: String by project
+val commons_codec_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.10"
@@ -33,12 +34,14 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     implementation("org.litote.kmongo:kmongo:$kmongo_version")
     implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
     implementation("org.litote.kmongo:kmongo-async:$kmongo_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
     implementation("org.litote.kmongo:kmongo-reactor:$kmongo_version")
     implementation ("com.stripe:stripe-java:$java_version")
+    implementation("commons-codec:commons-codec:$commons_codec_version")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
