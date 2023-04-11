184547c6c4d03e11ed2e31b691fd9f9598c47f44
nickdferrara <nick.d.ferrara@gmail.com>
2023-04-10 20:06:27 -0400

    Code clean up

--- src/main/kotlin/com/nickdferrara/extension/LocationExtension.kt
+++ src/main/kotlin/com/nickdferrara/extension/LocationExtension.kt
@@ -6,13 +6,13 @@ import com.nickdferrara.models.Location
 fun Location.toDto(): LocationDto =
     LocationDto(
         _id = this._id,
+        description = this.description,
         address = this.address.toDto(),
-        description = this.description
     )
 
 fun LocationDto.toLocation(): Location =
     Location(
         _id = this._id,
-        address = this.address.toAddress(),
-        description = this.description
+        description = this.description,
+        address = this.address.toAddress()
     )
--- src/main/kotlin/com/nickdferrara/models/Location.kt
+++ src/main/kotlin/com/nickdferrara/models/Location.kt
@@ -2,6 +2,6 @@ package com.nickdferrara.models
 
 data class Location(
     var _id: String? = null,
-    val address: Address,
     val description: String,
+    val address: Address,
 )
